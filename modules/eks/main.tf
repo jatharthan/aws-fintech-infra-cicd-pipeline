96c9e2486e1e31591b44a1dd4bfa2fa678b3ebf4
jatharthan <jatharthan94@gmail.com>
2025-11-19 09:36:02 -0500

    Implement EKS Node Group resource configuration in main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -75,30 +75,27 @@ resource "aws_eks_cluster" "this" {
 # -------------------
 # EKS Node Group
 # -------------------
-# resource "aws_eks_node_group" "this" {
-#   cluster_name    = aws_eks_cluster.this.name
-#   node_group_name = "${var.cluster_name}-node-group-${var.environment}"
-#   node_role_arn   = aws_iam_role.eks_node_role.arn
-#   subnet_ids      = var.private_subnet_ids
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "${var.cluster_name}-node-group-${var.environment}"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = var.private_subnet_ids
 
-#   scaling_config {
-#     desired_size = var.desired_capacity
-#     max_size     = var.max_size
-#     min_size     = var.min_size
-#   }
+  scaling_config {
+    desired_size = var.desired_capacity
+    max_size     = var.max_size
+    min_size     = var.min_size
+  }
 
-#   instance_types = [var.node_instance_type]
+  instance_types = [var.node_instance_type]
 
-#   remote_access {
-#     # ec2_ssh_key               = "eks-key-${var.environment}"
-#     ec2_ssh_key               = "Linux_machine_key"
-#     source_security_group_ids = var.node_security_group_ids
-#   }
+  remote_access {
+    # ec2_ssh_key               = "eks-key-${var.environment}"
+    ec2_ssh_key               = "Linux_machine_key"
+    source_security_group_ids = var.node_security_group_ids
+  }
 
-#   depends_on = [
-#     aws_iam_role_policy_attachment.eks_node_role_attachment,
-#     aws_iam_role_policy_attachment.eks_node_role_attachment-1,
-#     aws_iam_role_policy_attachment.eks_node_role_attachment-2,
-#     aws_eks_cluster.this
-#   ]
-# }
+  depends_on = [
+    aws_eks_cluster.this
+  ]
+}
