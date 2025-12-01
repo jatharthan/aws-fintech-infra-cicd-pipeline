9c77adcc2af9231232a1db1c64f767b929569a35
jatharthan <jatharthan94@gmail.com>
2025-12-01 12:13:26 -0500

    testing cluster deployment

--- main.tf
+++ main.tf
@@ -6,14 +6,14 @@ module "networking" {
   environment    = var.environment
 }
 
-# module "eks" {
-#   source                 = "./modules/eks"
-#   project_prefix = var.project_prefix
-#   region         = var.region
-#   environment            = var.environment
-#   vpc_id                 = module.networking.vpc_id
-#   cluster_name           = "pulseagami-eks"
-#   private_subnet_ids     = module.networking.private_subnet_ids
-#   public_subnet_ids      = module.networking.public_subnet_ids
-#   node_security_group_ids = [module.networking.ec2_sg_id]  # EC2 SG from your VPC module
-# }
+module "eks" {
+  source                 = "./modules/eks"
+  project_prefix = var.project_prefix
+  region         = var.region
+  environment            = var.environment
+  vpc_id                 = module.networking.vpc_id
+  cluster_name           = "pulseagami-eks"
+  private_subnet_ids     = module.networking.private_subnet_ids
+  public_subnet_ids      = module.networking.public_subnet_ids
+  node_security_group_ids = [module.networking.ec2_sg_id]  # EC2 SG from your VPC module
+}
