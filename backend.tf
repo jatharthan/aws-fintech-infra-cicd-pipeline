3476b630beaf1909d0983c9a01d56be194e23413
jatharthan <jatharthan94@gmail.com>
2025-12-01 11:39:05 -0500

    Update S3 backend key in Terraform configuration to workspace

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "insp1-tf-state-pulseagami"
-    key            = "dev/terraform.tfstate"
+    key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "insp1-terraform-lock-table"
     encrypt        = true
